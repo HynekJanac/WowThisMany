4dcf66e56f0e43238b3e6075ea8cae3088838082
Hynek Janáč <54169394+HynekJanac@users.noreply.github.com>
2021-03-29 20:36:37 +0200

    Inital commit

---
+++ Dark_mode.js
@@ -0,0 +1,7 @@
+if (window.matchMedia('(prefers-color-scheme: dark)').matches){
+    document.documentElement.setAttribute("data-theme","dark")
+
+}
+else{
+    document.documentElement.setAttribute("data-theme","light")
+}
